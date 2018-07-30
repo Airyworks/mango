6a67ef9c46561a5c1eae8836982bb6523065da3c
you06 <you1474600@gmail.com>
2018-07-30 11:24:24 +0800

    🚧 update viewer

--- src/renderer/components/viewer/Nav.vue
+++ src/renderer/components/viewer/Nav.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="sc-nav" name="nav">
-    <IosArrowBackIcon h="60px" w="20px"/>
+    <div class="sc-nav-back" @click="back">
+      <IosArrowBackIcon h="60px" w="20px"/>
+    </div>
   </div>
 </template>
 
@@ -14,6 +16,9 @@
       return {}
     },
     methods: {
+      back() {
+        this.$router.push('documents')
+      }
     }
   }
 </script>
@@ -26,4 +31,14 @@
     background-color #f3f3f3
     grid-template-columns 60px 1fr 150px
     justify-content center
+    .sc-nav-back
+      width 60px
+      height 100%
+      text-align center
+      cursor pointer
+      transition 0.3s
+      &:hover
+        background-color #ccc
+        color #fff
+      
 </style>
